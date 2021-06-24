55b575db93e53072e13688649b3f6bb282bd4eef
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2021-06-24 21:51:52 +0100

    Added method to load cogs
    Just a method to load all cogs automatically

--- Bot Files/HelperBot.py
+++ Bot Files/HelperBot.py
@@ -1,1 +1,14 @@
+import discord
+import os
 
+from discord.ext import commands
+
+token = "TheTokenHere"
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix=".", case_insensitive=True,intents=intents)
+
+for filename in os.listdir(R"./cogs"):
+    if filename.endswith(".py"):
+       bot.load_extension(f"cogs.{filename[:-3]}")
+
+bot.run(token)
